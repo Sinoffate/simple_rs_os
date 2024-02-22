3c71bdbffb2897c42ba151588a3385c56ee01919
Jered W <sinoffate@hotmail.com>
2024-02-21 17:26:25 -0800

    Update _start function to print "Hello World!" and panic with a message

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,16 @@ static HELLO: &[u8] = b"Hello World!";
 // w/o this attribute, the function would generate a name like _ZN4core3ptr18
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
