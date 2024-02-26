ddf3815b88f5bef5ee5e119eb13c12ba1be774f0
Jered W <sinoffate@hotmail.com>
2024-02-26 13:44:02 -0800

    Add Testable trait and implement it for functions

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,21 @@ mod serial;
 
 static HELLO: &[u8] = b"Hello World!";
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 // Disable name mangling to ensure that the Rust compiler really outputs a function with the name _start
 // w/o this attribute, the function would generate a name like _ZN4core3ptr18
 #[no_mangle]
@@ -60,10 +75,10 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
 
     exit_qemu(QemuExitCode::Success);
@@ -71,7 +86,6 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(0, 1);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
+
