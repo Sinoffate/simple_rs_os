2ba38ded5902801f4ef65241d612ff1be7493287
Jered W <sinoffate@hotmail.com>
2024-03-03 16:23:15 -0800

    Add interrupt handling and initialization

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,15 @@ use simple_rs_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    simple_rs_os::init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
+
+    println!("It did not crash!");
 
     loop {}
 }
